3f13ae32ce7802c10a6d1cca28a9f65fd18152ec
Andrei Petraru <phasola@gmail.com>
2013-08-07 16:53:48 +0300

    Updated binary search tests

--- src/test/groovy/fundamentals/BinarySearchTest.groovy
+++ src/test/groovy/fundamentals/BinarySearchTest.groovy
@@ -5,24 +5,32 @@ import fundamentals.BinarySearch
 
 class BinarySearchTest extends Specification {
 	def 'invalid arguments binary search'() {
-		BinarySearch.binarySearch(2, null) == -1
-		BinarySearch.binarySearch(-2, null) == -1
-		BinarySearch.binarySearch(-2, [1, 2, 3]) == -1
+		expect:
+		BinarySearch.binarySearch(key, array) == position
+		
+		where:
+		key | array   | position
+		 2	| null	  | -1
+		-2	| null	  | -1
+		-2	| [1,2,3] | -1		
 	}
 
 	def 'binary search'() {
 		expect:
-		BinarySearch.binarySearch(1, [1] as int[]) == 0
-		BinarySearch.binarySearch(2, [1, 2, 3] as int[]) == 1
-		BinarySearch.binarySearch(3, [1, 2, 3] as int[]) == 2
-		BinarySearch.binarySearch(2, [] as int[]) == -1
+		BinarySearch.binarySearch(key, array) == position
+		
+		where:
+		key | array   			 | position
+		 1	| [1] as int[] 		 |  0
+		 2	| [1, 2, 3] as int[] |  1
+		 3	| [1, 2, 3] as int[] |  2
+		 2	| [] as int[] 		 | -1
 	}
 
 	def 'large array binary search'() {
-		when:
-		int[] array = (0..10000000) as int[]
+		def array = (0..10000000) as int[]
 
-		then:
+		expect:
 		BinarySearch.binarySearch(0, array) == 0
 		BinarySearch.binarySearch(10000000.intdiv(2), array) == 10000000.intdiv(2)
 		BinarySearch.binarySearch(10000000.intdiv(2) - 12345, array) == 10000000.intdiv(2) - 12345
@@ -31,7 +39,7 @@ class BinarySearchTest extends Specification {
 	
 	def 'very large array binary search'() {
 		when:
-		int[] array = (0..10000000) as int[]
+		def array = (0..10000000) as int[]
 
 		then:
 		BinarySearch.binarySearch(0, array) == 0
