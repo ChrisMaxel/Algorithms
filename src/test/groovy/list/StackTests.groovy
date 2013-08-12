be561c7cff4348a528f503a6b5acfeb2d9ee2ac4
Andrei Petraru <phasola@gmail.com>
2013-08-12 12:00:11 +0300

    Stack Tests

---
+++ src/test/groovy/list/StackTests.groovy
@@ -0,0 +1,47 @@
+package list
+
+import spock.lang.Specification;
+import list.Stack;
+
+class StackTests extends Specification {
+
+	Stack stack = new Stack()
+
+	def 'push'() {
+		expect:
+		stack.push('string') == 'string'
+	}
+	
+	def 'push null'() {
+		when:
+		stack.push(null)
+		
+		then:
+		thrown IllegalArgumentException
+	}
+
+	def 'pop'() {
+		when:
+		stack.push('string')
+
+		then:
+		stack.pop() == 'string'
+		stack.pop() == null
+	}
+
+	def 'size'() {
+		when:
+		stack.push('string')
+
+		then:
+		stack.size() == 1
+	}
+
+	def 'clear'() {
+		when:
+		stack.clear()
+
+		then:
+		stack.isEmpty()
+	}
+}
