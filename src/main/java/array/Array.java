d0fd8609f6db393e38576ca0786017360e9ec4c0
Andrei Petraru <petraru@optymyze.com>
2014-04-11 11:55:39 +0300

    historgram + test

---
+++ src/main/java/array/Array.java
@@ -0,0 +1,35 @@
+package array;
+
+/**
+ * @author Andrei Petraru
+ * 10 Apr 2014
+ */
+public class Array {
+
+	/**
+	 * Write a static method histogram() that takes an array a[] of int values and
+	 * an integer M as arguments and returns an array of length M whose i-th entry is the number
+	 * of times the integer i appeared in the argument array. If the values in a[] are all
+	 * between 0 and  M–1, the sum of the values in the returned array should be equal to
+	 * a.length
+	 * @param a
+	 * @param m
+	 * @return
+	 */
+	public static int[] histogram(int[] a, int m) {
+		if (m < 0) {
+			throw new IllegalArgumentException("Cannot have an array with negative length");
+		}
+		int[] result = new int[m];
+		for (int i = 0; i < m; i++) {
+			int times = 0;
+			for (int j = 0; j < a.length; j++) {
+				if (i == a[j]) {
+					times++;
+				}
+			}
+			result[i] = times;
+		}
+		return result;
+	}
+}
---
+++ src/test/groovy/array/ArrayTests.groovy
@@ -0,0 +1,46 @@
+package array
+
+import spock.lang.Specification
+
+/**
+ * @author Andrei Petraru 
+ * 10 Apr 2014
+ */
+class ArrayTests extends Specification {
+	def 'test histogram with [0..m-1] values in array'() {
+		def array = [1, 2, 3, 3, 3, 3, 4] as int[]
+		def result = Array.histogram(array, 6)
+		def arraySum = 0
+		result.each {
+			print it
+			arraySum += it
+		}
+		println()
+
+		expect:
+		arraySum == array.length
+	}
+
+	def 'test histogram with larger values in array'() {
+		def array = [1, 12, 3, 2, 7, 3, 4] as int[]
+		def result = Array.histogram(array, 0)
+		def arraySum = 0
+		result.each {
+			print it
+			arraySum += it
+		}
+		println()
+
+		expect:
+		arraySum != array.length
+	}
+
+	def 'test histogram with negative M'() {
+		when:
+		Array.histogram([1, 2, 3] as int[], -1)
+
+		then:
+		def e = thrown(IllegalArgumentException)
+		e.message == 'Cannot have an array with negative length'
+	}
+}
