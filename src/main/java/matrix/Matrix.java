31d69e99e6510ac7441bd76e7e8354d5a5e7abb3
Andrei Petraru <petraru@optymyze.com>
2014-04-11 16:54:01 +0300

    relatively prime matrix + test

--- src/main/java/matrix/Matrix.java
+++ src/main/java/matrix/Matrix.java
@@ -1,5 +1,7 @@
 package matrix;
 
+import math.Arith;
+
 /**
  * @author Andrei Petraru
  * 10 Apr 2014
@@ -28,7 +30,7 @@ public class Matrix {
 	 *
 	 * @param a
 	 */
-	public static void print(int[][] a) {
+	public static void printMatrix(int[][] a) {
 		for (int i = 0; i < a.length; i++) {
 			System.out.print("[ ");
 			for (int j = 0; j < a[0].length; j++) {
@@ -37,5 +39,28 @@ public class Matrix {
 			System.out.print("]");
 			System.out.println();
 		}
+	}
+
+	/**
+	 * creates an N-by-N boolean array a[][] such that  a[i][j] is  true if
+	 * i and  j are relatively prime (have no common factors), and  false otherwise.
+	 *
+	 * @param n size of the created array
+	 * @return the relatively prime array
+	 */
+	public static boolean[][] relativePrime(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException("Array size cannot be negative");
+		}
+
+		boolean[][] result = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (Arith.gcd(i, j) == 1) {
+					result[i][j] = true;
+				}
+			}
+		}
+		return result;
 	}
 }
--- src/test/groovy/matrix/MatrixTests.groovy
+++ src/test/groovy/matrix/MatrixTests.groovy
@@ -11,7 +11,24 @@ class MatrixTests extends Specification {
 	def 'print transposition'() {
 		def matrix = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]] as int[][]
 		printTransposition(matrix)
-		Matrix.print(matrix)
+		printMatrix(matrix)
+
+		expect:
+		true
+	}
+
+	def 'create relatively prime matrix'() {
+		def matrix = relativePrime(10)
+
+		matrix.each {
+			print '['
+			it.each {
+				print it
+				print ' '
+			}
+			print ']'
+			println()
+		}
 
 		expect:
 		true
